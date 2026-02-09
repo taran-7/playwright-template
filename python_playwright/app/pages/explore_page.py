8a1458045f99dd516c906b85cb3d2e2b8b2a487a
Olex Taraniuk <taran76@gmail.com>
2026-02-09 14:40:13 +0200

    refactor: Improve code formatting and readability in ExplorePage class

--- python_playwright/app/pages/explore_page.py
+++ python_playwright/app/pages/explore_page.py
@@ -4,31 +4,47 @@ from python_playwright.app.base_page import BasePage
 from python_playwright.app.components.notification import NotificationComponent
 from python_playwright.app.components.header import HeaderComponent
 
+
 class ExplorePage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
         self.notification_component = NotificationComponent(page)
         self.header_component = HeaderComponent(page)
-        
+
         self.explore_url = "/explore"
-        
+
         # Locators
         self.search_field = self.page.locator('[data-testid="search"]')
-        self.filter_button = self.page.locator('[data-testid="portal-presentation-filter"]')
-        self.sort_option = self.page.locator('[data-testid="portal-presentation-sorting"]')
-        self.saved_option_link = self.page.locator('[data-testid="explore-page-bookmarks-navigation-option"]')
-        self.explore_link = self.page.locator('[data-testid="explore-page-navigation-option"]')
-        self.presentations_list = self.page.locator('[data-testid="presentations-grid-container"]')
-        self.message_something_went_wrong = self.page.get_by_role("heading", name="Something went wrong")
+        self.filter_button = self.page.locator(
+            '[data-testid="portal-presentation-filter"]'
+        )
+        self.sort_option = self.page.locator(
+            '[data-testid="portal-presentation-sorting"]')
+        self.saved_option_link = self.page.locator(
+            '[data-testid="explore-page-bookmarks-navigation-option"]')
+        self.explore_link = self.page.locator(
+            '[data-testid="explore-page-navigation-option"]')
+        self.presentations_list = self.page.locator(
+            '[data-testid="presentations-grid-container"]')
+        self.message_something_went_wrong = self.page.get_by_role(
+            "heading", name="Something went wrong")
 
     def presentation_card_by_index(self, index: int) -> Locator:
-        return self.page.locator('[data-testid="explore-page-presentation-card"]').nth(index)
+        return self.page.locator(
+            '[data-testid="explore-page-presentation-card"]'
+        ).nth(index)
 
     def presentation_card_by_title(self, title: str) -> Locator:
-        return self.page.locator('[data-testid="explore-page-presentation-card"]').filter(has_text=title).first
+        return (
+            self.page.locator(
+                '[data-testid="explore-page-presentation-card"]'
+            ).filter(has_text=title).first
+        )
 
     def views_counter_in_presentation_card(self, index: int) -> Locator:
-        return self.page.locator('[data-testid="explore-page-view-counter-presentation-card"]').nth(index)
+        return self.page.locator(
+            '[data-testid="explore-page-view-counter-presentation-card"]'
+        ).nth(index)
 
     def click_on_filter_button(self):
         self.filter_button.click()
